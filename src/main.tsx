d020ee5f312294c90995af2acc5b83acbe76134f
Glevin Roche <glevinroche@gmail.com>
2025-04-23 18:29:28 +0530

    Initial project_work commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HashRouter> {/* Wrap your App with HashRouter */}
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
